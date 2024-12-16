53198147c5cb802fd2de2d159c464566e972794a
Oliver <oschi1303@gmail.com>
2024-12-16 19:57:24 +0100

    Initial Commit

---
+++ MorgenPruefung/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MorgenPruefung
+//
+//  Created by Oliver Schlüter on 16.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MorgenPruefung/MorgenPruefungApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorgenPruefungApp.swift
+//  MorgenPruefung
+//
+//  Created by Oliver Schlüter on 16.12.24.
+//
+
+import SwiftUI
+
+@main
+struct MorgenPruefungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
